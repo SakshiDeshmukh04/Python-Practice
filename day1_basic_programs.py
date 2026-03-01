9f45c8cae12b8df5db31b2d3cb16293ef4b2817e
Sakshi Deshmukh <deshmukhsakshi707@gmail.com>
2026-03-01 22:02:19 +0530

    Day 1 Python practice programs added

---
+++ day1_basic_programs.py
@@ -0,0 +1,16 @@
+# Day 1 Python Practice
+
+# Print statement
+print("Hello GitHub")
+
+# Addition
+a = 10
+b = 5
+print("Sum:", a + b)
+
+# Even or Odd
+num = 7
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
